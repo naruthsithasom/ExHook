679f5c5f6c9175f52b292794bb18c4e34257bb41
naruthsithasom <ett0088@gmail.com>
2021-01-15 18:14:36 +0700

    hook useState

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Todo from './todoClass/todo'
 import { Switch, Route } from 'react-router-dom'
 import { Counter } from './hookCounter/Counter'
 import { TodoList } from './todoHook/TodoList'
-import { CounterNumber } from './Component/CounterNumber';
+import { CounterNumber } from './component/CounterNumber';
 
 function App() {
   return (
     <Switch>
-      <Route exact path='/'><div className="App">
+      <Route exact path='/'>
+        <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
